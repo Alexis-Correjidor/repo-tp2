c94e49d6c15604db43684394809b78bdc6230f2f
Alexis Correjidor <alexisgc.23@gmail.com.ar>
2023-05-21 22:53:56 -0300

    Implementacion Modificar y Eliminar datos en Controlador pagina serviciosPaseo

---
+++ Tp2_Grupo15/src/main/java/ar/edu/unju/fi/listas/ListaPaseador.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.listas;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.model.Paseador;
+
+
+public class ListaPaseador {
+
+	private List<Paseador>paseadores;
+	
+	public ListaPaseador() {
+		paseadores = new ArrayList<Paseador>();
+		paseadores.add(new Paseador("Juan", "Perez", "Lunes", 9, 12,16,18));
+		paseadores.add(new Paseador("Lucas", "Diaz", "Martes", 9, 12,16,18));
+		paseadores.add(new Paseador("Maria", "Gomez", "Jueves",9, 12, 16, 18));
+		
+	}
+	
+	public List<Paseador> getPaseadores() {
+		return paseadores;
+	}
+
+	public void setPaseadores(List<Paseador> paseadores) {
+		this.paseadores = paseadores;
+	}
+	
+}
